2dc617806c1dc128fced1140c431acdecae0dc90
Egor Panasenko <gaura.panasenko@gmail.com>
2019-06-05 11:52:09 +0300

    Lab 743

---
+++ 743/header.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1,
+			shrink-to-fit=no">
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet"
+			href="/css/bootstrap.min.css"
+			integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+			crossorigin="anonymous">
+
+		<!-- Font Awesome CSS -->
+		<link href="/css/all.min.css" rel="stylesheet">
+
+		<!-- Optional JavaScript -->
+		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+		<script src="/js/jquery-3.3.1.slim.min.js"
+		integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+		crossorigin="anonymous"></script>
+		<script src="/js/popper.min.js"
+		integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+		crossorigin="anonymous" defer></script>
+		<script src="/js/bootstrap.min.js"
+		integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+		crossorigin="anonymous" defer></script>
+		<script type="text/javascript" defer>
+			$(document).ready(function(){$('[data-toggle="tooltip"]').tooltip()});
+		</script>
+
+		<title><?=$title?></title>
+	</head>
+	<body>
+		<nav class="navbar navbar-expand-lg navbar-light bg-light shadow-sm">
+			<div class="container">
+				<a class="navbar-brand" href="<?=$base_path?>">Лабораторна робота №3</a>
+				<button class="navbar-toggler" type="button"
+					data-toggle="collapse" data-target="#navbarSupportedContent"
+					aria-controls="navbarSupportedContent" aria-expanded="false"
+					aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse"
+					id="navbarSupportedContent">
+					<ul class="navbar-nav mr-auto">
+						<li class="nav-item">
+							<a class="nav-link" href="<?=$base_path?>">Головна</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+		<div class="container">
+			<h1><?=$heading?></h1>
---
+++ 743/list.php
@@ -0,0 +1,122 @@
+<?php
+
+function print_error() {
+	include "header.php"; ?>
+		<div class="alert alert-secondary" role="alert">
+			Виникла помилка при завантаженні списку елементів.
+		</div>
+	<?php include "footer.php";
+	exit();
+}
+
+function format_data($id = NULL, $raw_data) {
+	$filtered = array_filter($raw_data, function($value) use ($id) {
+		return $value[1] == $id;
+	});
+	foreach($filtered as &$value) {
+		$value[3] = format_data($value[0], $raw_data);
+	}
+	return $filtered;
+}
+
+function get_root_path($id) {
+	global $mysqli;
+	$data = NULL;
+	while ($id) {
+		$query = "SELECT `id`, `parent_id`, `name` FROM `tree` WHERE `id`=$id";
+		if ($result = $mysqli->query($query)) {
+			$dt = $result->fetch_all();
+			$dt[0][3] = $data;
+			$data = $dt;
+			$id = $data[0][1];
+		} else {
+			print_error();
+		}
+	}
+	return $data;
+}
+
+function get_childs($id = NULL) {
+	global $mysqli;
+	$query = "SELECT `id`, `parent_id`, `name` FROM `tree` WHERE `parent_id`=$id";
+	if (!($result = $mysqli->query($query))) {
+		print_error();
+	}
+	$data = $result->fetch_all();
+	foreach($data as &$value) {
+		$value[3] = get_childs($value[0]);
+	}
+	return $data;
+}
+
+function print_data($data) {
+	global $base_path;
+	$id1 = !empty($_REQUEST["id1"]) ? $_REQUEST["id1"] : "";
+	$id2 = !empty($_REQUEST["id2"]) ? $_REQUEST["id2"] : "";
+	?>
+	<ul>
+	<?php foreach($data as $value) { ?>
+		<li>
+			<?=$value[2]?>
+			<a href="<?=$base_path?>/edit?id=<?=$value[0]?>"
+			data-toggle="tooltip" title="Змінити елемент"><i class="fa fa-edit"></i></a>
+			<a href="<?=$base_path?>/edit?parent_id=<?=$value[0]?>"
+			data-toggle="tooltip" title="Додати дочірній елемент"><i class="fa fa-plus-circle"></i></a>
+			<a href="<?=$base_path?>/delete?id=<?=$value[0]?>"
+			data-toggle="tooltip" title="Видалити елемент"><i class="fa fa-trash"></i></a>
+			<a href="<?=$base_path?>/list?mode=root&id=<?=$value[0]?>"
+			data-toggle="tooltip" title="Знайти шлях до кореневого елементу"><i class="fa fa-square-root-alt"></i></a>
+			<a href="<?=$base_path?>/list?mode=childs&id=<?=$value[0]?>"
+			data-toggle="tooltip" title="Вивести усі дорічні елементи"><i class="fa fa-leaf"></i></a>
+			<a href="<?=$base_path?>/list?mode=path&id1=<?=$value[0]?>&id2=<?=$id2?>"
+			data-toggle="tooltip" title="Знайти шлях з цього елементу"><i class="fa fa-fast-backward"></i></a>
+			<a href="<?=$base_path?>/list?mode=path&id1=<?=$id1?>&id2=<?=$value[0]?>"
+			data-toggle="tooltip" title="Знайти шлях до цього елементу"><i class="fa fa-fast-forward"></i></a>
+		<?php print_data($value[3]); ?>
+		</li>
+	<?php } ?>
+	</ul>
+<?php }
+
+$heading = $heading . " <a href='$base_path/edit' data-toggle='tooltip' title='Додати новий елемент'><i class='fa fa-plus-circle'></i></a>";
+if ($_REQUEST["mode"] == "root" and !empty($_REQUEST["id"]) and is_numeric($_REQUEST["id"]) and intval($_REQUEST["id"]) != 0) {
+	$id = intval($_REQUEST["id"]);
+	$data = get_root_path($id);
+} elseif ($_REQUEST["mode"] == "childs" and !empty($_REQUEST["id"]) and is_numeric($_REQUEST["id"]) and intval($_REQUEST["id"]) != 0) {
+	$id = $_REQUEST["id"];
+	$query = "SELECT `id`, `parent_id`, `name` FROM `tree` WHERE `id`=$id";
+	if (!($result = $mysqli->query($query))) {
+		print_error();
+	}
+	$data = $result->fetch_all();
+	$data[0][3] = get_childs($id);
+} elseif ($_REQUEST["mode"] == "path" and !empty($_REQUEST["id1"]) and is_numeric($_REQUEST["id1"]) and intval($_REQUEST["id1"]) != 0 and !empty($_REQUEST["id2"]) and is_numeric($_REQUEST["id2"]) and intval($_REQUEST["id2"]) != 0) {
+	$id1 = intval($_REQUEST["id1"]);
+	$id2 = intval($_REQUEST["id2"]);
+	$arr1 = get_root_path($id1);
+	$arr2 = get_root_path($id2);
+	while (!empty($arr1[0][3][0][0]) and !empty($arr2[0][3][0][0]) and $arr1[0][3][0][0] == $arr2[0][3][0][0]) {
+		$arr1 = $arr1[0][3];
+		$arr2 = $arr2[0][3];
+	}
+	if ($arr1[0][0] == $arr2[0][0]) {
+		$data = $arr1;
+		$data[0][3] = array_merge(empty($arr1[0][3]) ? array() : $arr1[0][3], empty($arr2[0][3]) ? array() : $arr2[0][3]);
+	} else {
+		$data = array_merge($arr1, $arr2);
+	}
+} else {
+	$query = "SELECT `id`, `parent_id`, `name` FROM `tree`";
+	if ($result = $mysqli->query($query)) {
+		$raw_data = $result->fetch_all();
+		$result->free();
+	} else {
+		print_error();
+	}
+	$data = format_data(NULL, $raw_data);
+}
+include "header.php";
+//~ print_r($data);
+print_data($data);
+include "footer.php";
+?>
