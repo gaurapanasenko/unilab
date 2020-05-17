9f37a5fe6ed0e16c16f63a902a3b918e0042093a
Yehor Panasenko <yehor.panasenko@gmail.com>
2020-05-18 00:53:08 +0300

    Unix_Lab2

--- 06/Unix_Lab2/lab2.sh
+++ 06/Unix_Lab2/lab2.sh
@@ -3,11 +3,13 @@
 DIRECTORY="$1"
 
 [ ! -d "$DIRECTORY" ] && echo "No such directory: \"$DIRECTORY\"" && exit 1
+INDEX="$DIRECTORY/index"
 
-if [ -f index ]; then ./index
-elif [ -f index.html ]; then gtkmoz index.html
-elif [ -f index.htm ]; then bareview index.html
+if [ -f "$INDEX" ]; then "$INDEX"
+elif [ -f "$INDEX.html" ]; then gtkmoz "$INDEX.html"
+elif [ -f "$INDEX.htm" ]; then bareview "$INDEX.htm"
 else
     FILE=`Xdialog --stdout --title "Виберіть файл" --fselect "$DIRECTORY" 50 150`
     [ -f $FILE ] && $FILE
 fi
+exit $?
