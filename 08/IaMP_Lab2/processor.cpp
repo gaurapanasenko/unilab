305e1232d7b6262360ca9b2606bb078dc2b9ca42
Yehor Panasenko <yehor.panasenko@gmail.com>
2021-03-05 03:28:30 +0200

    IaMP_Lab2 - Refactoring 2 (Processor class)

--- 08/IaMP_Lab2/processor.cpp
+++ 08/IaMP_Lab2/processor.cpp
@@ -4,3 +4,20 @@ Processor::Processor(shared_ptr<const ImageData> input)
     : orig(input), data(input) {
     updateDissection();
 }
+
+void Processor::updateDissection() {
+    if (dissected) {
+        float diffY = dissection_y[1] - dissection_y[0];
+        int diffX = dissection_x[1] - dissection_x[0];
+        float diff = diffY / diffX;
+        float acc = dissection_y[0];
+        memset(dissection, 0, 256 * sizeof(float));
+        for (int i = dissection_x[0]; i < dissection_x[1]; i++) {
+            acc += diff;
+            dissection[i] = acc;
+        }
+        data = make_shared<ImageData>(dissect(*orig->image, dissection));
+    } else
+        data = orig;
+    texture.update(*data->image);
+}
--- 08/IaMP_Lab2/processor.h
+++ 08/IaMP_Lab2/processor.h
@@ -13,26 +13,8 @@ public:
     float dissection_y[2] = {0, 1};
     float dissection[256] = {0};
     bool dissected;
-    Processor(shared_ptr<const ImageData> input)
-        : orig(input), data(input) {
-        updateDissection();
-    }
-    void updateDissection() {
-        if (dissected) {
-            float diffY = dissection_y[1] - dissection_y[0];
-            int diffX = dissection_x[1] - dissection_x[0];
-            float diff = diffY / diffX;
-            float acc = dissection_y[0];
-            memset(dissection, 0, 256 * sizeof(float));
-            for (int i = dissection_x[0]; i < dissection_x[1]; i++) {
-                acc += diff;
-                dissection[i] = acc;
-            }
-            data = make_shared<ImageData>(dissect(*orig->image, dissection));
-        } else
-            data = orig;
-        texture.update(*data->image);
-    }
+    Processor(shared_ptr<const ImageData> input);
+    void updateDissection();
 };
 
 #endif // PROCESSOR_H
